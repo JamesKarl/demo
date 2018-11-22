077f7653034e9b23800cdae613d33a46ceee4d4a
JamesKarl <kejun@wiseda.com.cn>
2018-11-22 18:11:13 +0800

    add mine fragment.

--- app/src/main/java/com/xsuperman/basic/MainViewModel.kt
+++ app/src/main/java/com/xsuperman/basic/MainViewModel.kt
@@ -1,16 +1,14 @@
 package com.xsuperman.basic
 
 import androidx.lifecycle.ViewModel
-import com.xsuperman.basic.home.DashboardFragment
-import com.xsuperman.basic.home.HomeFragment
-import com.xsuperman.basic.home.PageContent
-import com.xsuperman.basic.home.WorkFragment
+import com.xsuperman.basic.home.*
 
 class MainViewModel : ViewModel() {
 
     val pageContents = listOf(
         PageContent(R.id.navigation_home) { HomeFragment.newInstance(1) },
         PageContent(R.id.navigation_dashboard) { DashboardFragment.newInstance() },
-        PageContent(R.id.navigation_notifications) { WorkFragment.newInstance("a", "b") }
+        PageContent(R.id.navigation_notifications) { WorkFragment.newInstance("a", "b") },
+        PageContent(R.id.navigation_mine) { MineFragment.newInstance("a", "b") }
     )
 }
