7bf362ef9affaa47c27cff737624598082177479
James Karl <jameskarl2@163.com>
2019-01-12 14:37:21 +0800

    add some TabLayout

--- app/src/main/java/com/xsuperman/basic/home/mine/MineFragment.kt
+++ app/src/main/java/com/xsuperman/basic/home/mine/MineFragment.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 
 import com.xsuperman.basic.R
+import kotlinx.android.synthetic.main.mine_fragment_mine.*
 
 class MineFragment : Fragment() {
 
