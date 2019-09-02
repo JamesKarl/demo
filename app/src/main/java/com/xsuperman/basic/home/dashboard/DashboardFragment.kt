eb2ccbd786837dce8d5eb72759ab164c7423cc45
James Karl <jameskarl2@163.com>
2019-09-02 19:42:32 +0800

    add OcrActivity

--- app/src/main/java/com/xsuperman/basic/home/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/xsuperman/basic/home/dashboard/DashboardFragment.kt
@@ -1,5 +1,6 @@
 package com.xsuperman.basic.home.dashboard
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,6 +8,8 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import com.xsuperman.basic.R
+import com.xsuperman.basic.ml.ocr.OcrActivity
+import kotlinx.android.synthetic.main.home_dashboard_fragment.*
 
 class DashboardFragment : Fragment() {
 
@@ -26,6 +29,10 @@ class DashboardFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(DashboardViewModel::class.java)
+
+        gotoOcr.setOnClickListener {
+            startActivity(Intent(this.activity, OcrActivity::class.java))
+        }
     }
 
 }
